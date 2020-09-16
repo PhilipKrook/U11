d67e3414cd3e540ee21f32f2edf40a2d8837497b
Philip Krook <philip.krook@chasacademy.se>
2020-09-16 16:23:18 +0200

    added bootstrap and router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,56 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { MemoryRouter, Switch, Route } from "react-router";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import ButtonToolbar from "react-bootstrap/ButtonToolbar";
+import { LinkContainer } from "react-router-bootstrap";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+
+const Home = () => <span>Home</span>;
+const About = () => <span>About</span>;
+const Users = () => <span>Users</span>;
+
+/* const App = () => (
+  <MemoryRouter>
+    <Container className="p-3">
+      <Jumbotron>
+        <h1 className="header">Welcome To React-Bootstrap</h1>
+        <h2>
+          Current Page is{" "}
+          <Switch>
+            <Route path="/about"><About />
+            </Route>
+            <Route path="/users"><Users />
+            </Route>
+            <Route path="/"><Home />
+            </Route>
+          </Switch>
+        </h2>
+        <h2>
+          Navigate to{" "}
+          <ButtonToolbar className="custom-btn-toolbar">
+            <LinkContainer to="/"><Button>Home</Button>
+            </LinkContainer>
+            <LinkContainer to="/about"><Button>About</Button>
+            </LinkContainer>
+            <LinkContainer to="/users"><Button>Users</Button>
+            </LinkContainer>
+          </ButtonToolbar>
+        </h2>
+      </Jumbotron>
+    </Container>
+  </MemoryRouter>
+);
+
+export default App; */
 
 // ** Calendar **
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 
-function MyApp() {
+ function MyApp() {
   const [value, setValue] = useState(new Date());
   function onChange(nextValue) {
     setValue(nextValue);
@@ -17,5 +62,5 @@ function MyApp() {
     />
   );
 }
-export default MyApp;
+export default MyApp; 
 // ** End Calendar **
